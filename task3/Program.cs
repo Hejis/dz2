4334dbbe7b6796459558f615517a5d623456730e
simpaa_comp <simpaa@inbox.ru>
2022-09-10 11:10:41 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+if (anyNumber < 1000)
+ {
+    int result = (anyNumber/100)%10;
+    if (result != 0 ){
+    Console.WriteLine("третья цифра -> " + result);
+    }
+    else {
+    Console.WriteLine("-> третьей цифры нет");
+    }
+ }
+else {
+    Console.WriteLine("Ваше число не трехзначное");
+}
+
