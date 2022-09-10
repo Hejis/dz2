873292ddcf5c16e881bcae1836086b070d73f766
simpaa_comp <simpaa@inbox.ru>
2022-09-10 10:48:18 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное 
+//число и удаляет вторую цифру этого числа.
+Random rnd = new Random();
+int NumberValue= rnd.Next(100,999);
+int result = (NumberValue % 10) + (NumberValue / 100) * 10;
+Console.WriteLine($"Для числа {NumberValue} при удалении второго  разряда получаем {result} ");
